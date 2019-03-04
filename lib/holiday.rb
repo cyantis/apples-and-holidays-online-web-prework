fd1ccc39b07e3a6798d2f4edb863754634e607b9
Cody Yantis <cyantis@gmail.com>
2019-03-04 02:00:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, holidays|
+  holiday_hash.collect do |season, holidays|
     holidays.collect do |holiday, supplies|
       holiday if supplies.include?("BBQ")
     end
